39b91bb7804d95736207543424fb50c7002b36c7
jinx <jinxh29224@hs.handsome.com.cn>
2020-04-10 17:37:57 +0800

    123

---
+++ AutoTestNG_Server/src/main/java/com/testfile/SpringbootApplication.java
@@ -0,0 +1,18 @@
+package com.testfile;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+		
+	
+	}
+
+}
