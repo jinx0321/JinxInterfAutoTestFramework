eaab557eb630519f6228602f2a2f945129f671b4
jinx <jinxh29224@hs.handsome.com.cn>
2020-05-20 09:53:58 +0800

    增加自动化测试demo工程

---
+++ TestProject/src/main/java/flow/form.java
@@ -0,0 +1,32 @@
+package flow;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import AutoTest.Base.BaseFlow;
+import AutoTest.Base.TestInfo;
+import AutoTest.DataAction.TestExecAction;
+import AutoTest.SuitInfo.HttpSuit.HttpExecAction;
+import AutoTest.SuitInfo.HttpSuit.HttpTestInfo;
+
+public class form extends BaseFlow<HttpTestInfo,HttpExecAction>{
+	final String url="http://localhost:9090/json";
+	
+	@DataProvider(name="data")
+	public Object[][] returnData(){
+	   return returnExcelData();	
+	}
+	
+	@Test(dataProvider ="data")
+	public void test(HttpTestInfo ti) throws Exception {
+		String result=(String)CaseExec(ti,new HttpExecAction(),url);
+		
+		System.out.println("·µ»ØÊý¾Ý:"+result);
+	}
+
+}
